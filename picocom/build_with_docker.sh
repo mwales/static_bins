c3b4faa1efe22cd5ba3c2b91e4d18ac3d434f9b7
Michael Wales <mwales3@gmail.com>
2024-06-21 03:20:01 -0400

    Fixed an issue where binaries created by docker container were owned by root

--- picocom/build_with_docker.sh
+++ picocom/build_with_docker.sh
@@ -5,6 +5,8 @@ docker build -t picocom-dev .
 ARCH_SELF=`uname -p`
 mkdir -p ../bins/$ARCH_SELF
 
-docker run -v ./:/tmp -it picocom-dev cp picocom/picocom /tmp
+docker run -v ./:/tmp -it picocom-dev cp picocom/picocom /tmp/temp_binary
+cp temp_binary picocom
+rm -f temp_binary
 mv picocom ../bins/$ARCH_SELF
 
